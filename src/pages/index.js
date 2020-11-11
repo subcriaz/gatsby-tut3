3a763fe0ce6c85efb9b790ce8e85b001cb759c3f
Riaz Hassan <subcriaz@yahoo.com>
2020-11-11 16:23:21 +0500

    final

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import React from "react"
 
 export default () => <div>
   
-  <p>
-    bahriatown-peshawar.com.pk </p><p> bahriatown-peshawar.pk </p>
-<p></p><P>Contact: Infosoft Services : iserv92@gmail.com </P>
+  <h1>
+    bahriatown-peshawar.com.pk </h1><h1> bahriatown-peshawar.pk </h1>
+<p></p><h2>Contact: Infosoft Services : iserv92@gmail.com </h2>
 
     </div>
