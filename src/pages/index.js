85b03fb3813e1027aff5ffd0e2ad1f400c720ee7
Riaz Hassan <subcriaz@yahoo.com>
2020-11-11 16:13:57 +0500

    btp3

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,8 +2,8 @@ import React from "react"
 
 export default () => <div>
   
-  <H1>
-    bahriatown-peshawar.com.pk </H1><H1> bahriatown-peshawar.pk </H1>
-<P>contact: Infosoft Services : iserv92@gmail.com </P>
+  <h1>
+    bahriatown-peshawar.com.pk </h1><h11> bahriatown-peshawar.pk </h1>
+<p/><P>contact: Infosoft Services : iserv92@gmail.com </P>
 
     </div>
