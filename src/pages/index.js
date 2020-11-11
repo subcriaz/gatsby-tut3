a4ab62e3f9ac6a6ebc4338637cb26f61c304b171
Riaz Hassan <subcriaz@yahoo.com>
2020-11-11 16:04:31 +0500

    btp

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,3 +1,9 @@
 import React from "react"
 
-export default () => <div>Hello world!</div>
+export default () => <div>
+  
+  Hello !
+    bahriatown-peshawar.com.pk & bahriatown-peshawar.pk
+contact: Infosoft Services : iserv92@gmail.com
+
+    </div>
